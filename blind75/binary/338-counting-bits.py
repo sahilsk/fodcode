eae2d1444333916421315fc0e78b9d3947bf8deb
sahilsk <sonukr666@gmail.com>
2022-05-27 22:46:37 +0100

    added binary

---
+++ blind75/binary/338-counting-bits.py
@@ -0,0 +1,55 @@
+"""
+338. Counting Bits - Easy
+
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+Example 1:
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+
+Example 2:
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+ 
+
+Constraints:
+
+0 <= n <= 105
+"""
+
+from runner import runner
+
+
+def countSetBits(n:int) -> int:
+    tsum = 0
+    for i in range(32):
+        if (n & (1 << i) != 0):
+            tsum += 1
+    return tsum
+
+
+def countBits(n:int) -> list[int]:
+    arrlen = n + 1
+    ans = [0] * arrlen
+    for i in range(arrlen):
+        ans[i] = countSetBits(i)
+    
+    return ans
+
+
+testcases = {
+    "[0,1,1]": (2,),
+    "[0,1,1,2,1,2]": (5,)
+}
+runner(countBits, testcases)
